c5314ba3506833154095dec20ecb5a15edc75020
zikreali0786 <76613713+zikreali0786@users.noreply.github.com>
2021-12-03 05:52:02 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -41,5 +41,6 @@ class _BotCommands:
         self.LeechWatchCommand = 'leechwatch'
         self.LeechZipWatchCommand = 'leechzipwatch'
         self.GdtotCommand = 'gdtot'
-        self.GDTOTCommand = 'gdtot'
+
+  
 BotCommands = _BotCommands()
